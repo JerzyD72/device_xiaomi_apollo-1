f47f62d829110171fa6dc89af5f3f5c6ce594888
Ramii Ahmed <ramy@ahmedramy.com>
2021-02-02 17:46:44 +0000

    apollo: import libinit from Miatoll tree & Adapt to Apollo Family
    https://github.com/ramisky/device_xiaomi_miatoll

--- lineage_apollo.mk
+++ lineage_apollo.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from Apollo device
+# Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
 # Inherit some common Lineage stuff.
@@ -18,11 +18,6 @@ PRODUCT_NAME := lineage_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Xiaomi Mi 10T
+PRODUCT_MODEL := Mi 10T
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="apollo-user 11 RKQ1.200826.002 20.12.25 release-keys"
-
-BUILD_FINGERPRINT := Redmi/apollo/apollo:11/RKQ1.200826.002/20.12.25:user/release-keys
