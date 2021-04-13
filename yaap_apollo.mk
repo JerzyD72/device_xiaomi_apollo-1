0283908a5fd9c75287768b279c385deebf2c942a
ejbtrd <ejbtrd@gmail.com>
2021-04-13 17:38:38 +0000

    apollo: YAAPpyn
    Signed-off-by: ejbtrd <ejbtrd@gmail.com>

--- yaap_apollo.mk
+++ yaap_apollo.mk
@@ -11,13 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common YAAP stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_apollo
+PRODUCT_NAME := yaap_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 10T
+
+scr_resolution := 1440
+TARGET_BUILD_GAPPS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
